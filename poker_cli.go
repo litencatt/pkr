18b4e3ad1f8431188ca08d8452d2ae35fb10cb43
中村 光佑 <nakamura.k@pepabo.com>
2024-08-10 23:02:58 +0900

    shorter

--- poker_cli.go
+++ poker_cli.go
@@ -145,9 +145,9 @@ func (cli *PokerCLI) Run() error {
 			fmt.Println()
 		}
 
-		roundResultStats := cli.service.GetRoundStats()
+		stats := cli.service.GetRoundStats()
 		if cli.service.IsRoundWin() {
-			fmt.Printf("Score at least: %d, Round score: %d\n", roundResultStats.ScoreAtLeast, roundResultStats.TotalScore)
+			fmt.Printf("Score at least: %d, Round score: %d\n", stats.ScoreAtLeast, stats.TotalScore)
 			fmt.Println("")
 
 			prompt := &survey.Select{
@@ -162,8 +162,8 @@ func (cli *PokerCLI) Run() error {
 			continue
 		}
 
-		if roundResultStats.Hands == 0 {
-			fmt.Printf("Score at least: %d, Round score: %d\n", roundResultStats.ScoreAtLeast, roundResultStats.TotalScore)
+		if stats.Hands == 0 {
+			fmt.Printf("Score at least: %d, Round score: %d\n", stats.ScoreAtLeast, stats.TotalScore)
 			fmt.Println("You lose!")
 			break
 		}
